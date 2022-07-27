63925a9220d24c9f2291d84587f76a1c17219130
baronkobama <66051646+baronkobama@users.noreply.github.com>
2022-07-27 14:13:04 -0500

    feat: split into index, projects, socials pages

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__, static_folder="resources", template_folder="pages")
 
 
 @app.route("/")
 def home():
+    return redirect("/index")
+
+
+@app.route("/index")
+def index():
     return render_template("index.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/socials")
+def socials():
+    return render_template("socials.html")
