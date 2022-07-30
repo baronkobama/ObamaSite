6a5f26f0e3830f0a7ea81dda20cbbbaee8115df0
baronkobama <66051646+baronkobama@users.noreply.github.com>
2022-07-29 22:19:13 -0500

    feat: flesh out basics of index.html/index.css

--- src/main.py
+++ src/main.py
@@ -1,18 +1,13 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="resources", template_folder="pages")
 
 
 @app.route("/")
-def base():
+def index():
     return render_template("index.html")
 
 
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
-
-
-@app.route("/socials")
-def socials():
-    return render_template("socials.html")
